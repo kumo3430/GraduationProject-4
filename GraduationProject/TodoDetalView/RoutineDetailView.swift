06f8406695a2a93200ff32a63d72bbdd2afa4ab5
Yun <3430yun>
2023-12-07 02:21:52 +0800

    作息

---
+++ GraduationProject/TodoDetalView/RoutineDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineDetailView.swift
+//  GraduationProject
+//
+//  Created by 呂沄 on 2023/12/7.
+//
+
+import SwiftUI
+
+struct RoutineDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutineDetailView()
+}
